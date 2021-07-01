39e91fa48c4630605d40c625e6567420b76bce98
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-01 14:57:25 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>{/* <Route path="/" exact={} /> */}</Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
